3cbde8c4e996e0e25e9cc795bb77caf28410ec1e
Andrei Chepushtanov <aschepushtanov@gmail.com>
2020-09-03 04:01:53 +0300

    feat: add simple SPA

---
+++ src/app.js
@@ -0,0 +1,82 @@
+const app = {
+    pages: [],
+    show: new Event('show'),
+    init: function(){
+        // Animation event
+        app.pages = document.querySelectorAll('.dynamic-header');
+        app.pages.forEach((pg) => {
+            pg.addEventListener('show', app.pageShown);
+        })
+
+        // Set active link
+        document.querySelectorAll('.nav-link').forEach((link) => {
+            link.addEventListener('click', app.setActiveLink)
+        })
+
+        // 
+        if (!location.hash) {
+            location.hash = '#about'
+        }
+
+        // Init content
+        app.getContent();
+
+        // History popstate
+        window.addEventListener('popstate', app.popState);
+    },
+    fetchFile: function(fragmentId, callback) {
+
+        if(fragmentId === 'pokemon'){
+            getPokemon('jigglypuff')
+            .then(function(value){
+                callback(value);
+            })
+        }
+        else {
+            fetch('content/' + fragmentId + '.html')
+            .then(function(response){
+                    return response.text()
+            })
+            .then(function(value){
+                callback(value);
+            });
+        }
+    },
+    getContent: function(){
+        var mainContent = document.getElementById('main');
+        fragmentId = location.hash.substr(1);
+        app.fetchFile(fragmentId, function(content){
+            mainContent.innerHTML = content;
+        });
+    },
+    setActiveLink: function(ev){
+        ev.preventDefault();
+        let currentPage = ev.target.getAttribute('data-target');
+        document.querySelector('.active').classList.remove('active');
+        document.getElementById(currentPage).classList.add('active');
+        history.pushState({}, currentPage, `#${currentPage}`);
+        document.getElementById(currentPage).dispatchEvent(app.show);
+        app.getContent();
+    },
+    // Update dynamic content
+    nav: function(ev){
+        return 0;
+    },
+    pageShown: function(ev){
+        let pokemon = ev.target.querySelectorAll('.header-img')[1]
+        pokemon.classList.add('img-flip');
+        setTimeout((poke) => {
+            poke.classList.remove('img-flip')
+        }, 1000, pokemon); 
+    },
+    popState: function(ev){
+        console.log(location.hash, 'popstate event');
+        let hash = location.hash.replace('#','');
+        document.querySelector('.active').classList.remove('active');
+        document.getElementById(hash).classList.add('active');
+        document.getElementById(hash).dispatchEvent(app.show);
+        app.getContent();
+    }
+}
+
+document.addEventListener('DOMContentLoaded', app.init)
---
+++ src/pokemon.js
@@ -0,0 +1,43 @@
+async function getAbilityInfo(abilityUrl) {
+    let response = await fetch(abilityUrl);
+    if (!response.ok) {
+        console.log('ERROR: Request returned with error');
+        return null;
+    }
+    const EFFECT_DATA = await response.json();
+    return EFFECT_DATA.effect_entries[1].effect;
+}
+
+function normalizeName(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+async function getPokemon(pokemonName) {
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+
+    if (!response.ok) {
+        return `<div class="error-page">
+                <img class="error-img" alt="" src="/img/404.jpg">
+                <p>404: Not found.<br>
+                Jiggly isn't here, so where is he?</p>
+            </div>`;
+    }
+
+    const POKEMON_DATA = await response.json();
+    
+    let pokeInfo = `<div class="jiggly-page">
+                    <img class="jiggly-img" alt="" src="${POKEMON_DATA.sprites.front_shiny}">
+                    <div class="jiggly-info">
+                    <p>${normalizeName(pokemonName)}</p>
+                    <ul class="abilities">`;
+
+    for (let i = 0; i < POKEMON_DATA.abilities.length; i++) {
+        let ability = POKEMON_DATA.abilities[i].ability;
+        let abilityUrl = ability.url;
+        let abilityPromise = getAbilityInfo(abilityUrl);
+        let abilityName = normalizeName(ability.name);
+        pokeInfo += `<li><span>${abilityName}</span><br>: ${await abilityPromise}<br><br></li>`;
+    }
+
+    return pokeInfo + `</ul></div>`;
+}
